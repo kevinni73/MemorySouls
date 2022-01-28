d074e9a293afe6a057a2f54f8a2ca2efc12daa1f
Kevin Ni <kevinni73@gmail.com>
2022-01-27 20:44:57 -0800

    Copy over assets, implement basic player movement and animations

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,232 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class Player : MonoBehaviour
+{
+    #region Variables
+
+    // Movement
+    [SerializeField] float _runSpeed = 8;
+
+    // Roll
+    bool _rolled = false;
+    [SerializeField] float _rollSpeed = 16;
+    [SerializeField] float _rollTime = 0.2f;
+    [SerializeField] float _rollCooldownTime = 0.25f;
+    float _rollCooldownTimer;
+    Vector2 _rollDir;
+
+    // Hit knockback
+    const float _knockbackSpeed = 10;
+    const float _knockbackTime = 0.1f;
+    const float _endKnockbackMult = 0.25f;
+    Vector2 _knockbackDir;
+
+    // General Movement
+    Vector2 _velocity;
+    Vector2 _moveInput;
+    Vector2 _lastDir;
+    float _lastDirectionFloat = 0.75f; // from 0-1, going clockwise from Vector.right
+
+    // Invincibility
+    const float _invincibilityDuration = 1f;
+    const float _respawnInvincibilityDuration = 0.5f;
+    const float _invincibilityFlashTime = 0.1f;
+    bool _invincible = false;
+
+    // Other components
+    SpriteRenderer _renderer;
+    StateMachine _stateMachine;
+    BoxCollider2D _collider;
+    Rigidbody2D _rb;
+    Animator _animator;
+
+    #endregion
+
+
+    #region Internal types
+    public enum State
+    {
+        Normal,
+        Roll,
+        Damaged,
+    };
+    #endregion
+
+
+    #region Monobehavior
+    void Awake()
+    {
+        _renderer = GetComponent<SpriteRenderer>();
+        _collider = GetComponent<BoxCollider2D>();
+        _animator = GetComponent<Animator>();
+        _rb = GetComponent<Rigidbody2D>();
+
+        _stateMachine = gameObject.AddComponent<StateMachine>();
+        _stateMachine.Init(Enum.GetNames(typeof(State)).Length);
+        _stateMachine.AddState((int)State.Normal, NormalUpdate, null, null, null);
+        _stateMachine.AddState((int)State.Roll, null, RollCoroutine, RollBegin, RollEnd);
+        _stateMachine.AddState((int)State.Damaged, null, DamagedCoroutine, DamagedBegin, DamagedEnd);
+    }
+
+    void Update()
+    {
+        _animator.SetFloat("SpeedX", _velocity.x);
+        _animator.SetFloat("SpeedY", _velocity.y);
+        _animator.SetFloat("Speed", _velocity.sqrMagnitude);
+        _animator.SetFloat("LastDir", _lastDirectionFloat);
+    }
+
+    private void FixedUpdate()
+    {
+        _rb.MovePosition(_rb.position + _velocity * Time.fixedDeltaTime);
+    }
+    #endregion
+
+
+    #region Normal State
+
+    int NormalUpdate()
+    {
+        if (_rolled)
+        {
+            _rolled = false;
+            return (int)State.Roll;
+        }
+
+        _velocity = _moveInput.normalized * _runSpeed;
+
+        return (int)State.Normal;
+    }
+    #endregion
+
+
+    #region Roll State
+
+    void RollBegin()
+    {
+        _animator.SetFloat("RollDir", DirectionToAngleRange01(_rollDir));
+        _animator.SetBool("Rolling", true);
+        _velocity = _rollDir.normalized * _rollSpeed;
+    }
+
+    void RollEnd()
+    {
+        _animator.SetBool("Rolling", false);
+        _velocity = Vector2.zero;
+    }
+
+    IEnumerator RollCoroutine()
+    {
+        yield return new WaitForSeconds(_rollTime);
+        _stateMachine.State = (int)State.Normal;
+    }
+
+    #endregion
+
+
+    #region Damaged State
+
+    void DamagedBegin()
+    {
+        _velocity = _knockbackDir.normalized * _knockbackSpeed;
+        BecomeInvincibile(_invincibilityDuration);
+    }
+
+    void DamagedEnd()
+    {
+        _velocity *= _endKnockbackMult;
+    }
+
+    IEnumerator DamagedCoroutine()
+    {
+        yield return new WaitForSeconds(_knockbackTime);
+        _stateMachine.State = (int)State.Normal;
+    }
+
+    #endregion
+
+
+    #region Inputs
+
+    void OnMove(InputValue value)
+    {
+        _moveInput = value.Get<Vector2>();
+
+        if (_moveInput != Vector2.zero)
+        {
+            _lastDir = _moveInput.normalized;
+            _lastDirectionFloat = DirectionToAngleRange01(_moveInput);
+        }
+
+        //Debug.Log(_moveInput);
+    }
+
+    void OnRoll(InputValue value)
+    {
+        _rollDir = _lastDir;
+        _rolled = true;
+    }
+
+    #endregion
+
+
+    #region Public Player API
+
+    public void TakeDamage(int damage, Vector2 knockback)
+    {
+        if (_invincible)
+        {
+            return;
+        }
+
+        if (_stateMachine.State != (int)State.Damaged)
+        {
+            _knockbackDir = knockback;
+            _stateMachine.State = (int)State.Damaged;
+        }
+    }
+
+    #endregion
+
+
+    #region Private Player Methods
+
+    // Gets the angle of a vector and transform it to a range of [0, 1),
+    // starting from Vector.right and going counterclockwise
+    float DirectionToAngleRange01(Vector2 direction)
+    {
+        float angle = Mathf.Atan2(_moveInput.y, _moveInput.x) / (Mathf.PI * 2);
+        if (angle < 0)
+        {
+            angle += 1;
+        }
+        return angle;
+    }
+
+    void BecomeInvincibile(float invicibilityDuration)
+    {
+        IEnumerator coroutine = InvincibilityCoroutine(invicibilityDuration);
+        StartCoroutine(coroutine);
+    }
+
+    IEnumerator InvincibilityCoroutine(float invincibilityDuration)
+    {
+        _invincible = true;
+        bool flash = true;
+
+        for (float timer = 0; timer < invincibilityDuration; timer += _invincibilityFlashTime)
+        {
+            _renderer.color = flash ? Color.red : Color.white;
+            flash = !flash;
+            yield return new WaitForSeconds(_invincibilityFlashTime);
+        }
+
+        _renderer.color = Color.white;
+        _invincible = false;
+    }
+    #endregion
+}
---
+++ Assets/Scripts/StateMachine.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StateMachine : MonoBehaviour
+{
+    struct StateInfo
+    {
+        public int state;
+        public Func<int> updateFunc;
+        public Func<IEnumerator> coroutineFunc;
+        public Action beginFunc;
+        public Action endFunc;
+    };
+
+    private List<StateInfo> _states;
+    [SerializeField] private int _state = 0;
+    public int State
+    {
+        get => _state;
+        set
+        {
+            TransitionState(value, true);
+        }
+    }
+
+    private IEnumerator _coroutine;
+
+    public void Init(int size)
+    {
+        _states = new List<StateInfo>(new StateInfo[size]);
+    }
+
+    public void AddState(int state, Func<int> updateFunc, Func<IEnumerator> coroutineFunc, Action beginFunc, Action endFunc)
+    {
+        StateInfo info = new StateInfo { state = state, updateFunc = updateFunc, coroutineFunc = coroutineFunc, beginFunc = beginFunc, endFunc = endFunc };
+        _states[(int)state] = info;
+    }
+
+    private StateInfo currentState()
+    {
+        return _states[(int)_state];
+    }
+
+    private void Start()
+    {
+        if (currentState().beginFunc != null)
+        {
+            currentState().beginFunc();
+        }
+    }
+
+    private void TransitionState(int state, bool force = false)
+    {
+        if (!force && _state == state)
+        {
+            return;
+        }
+
+        if (_coroutine != null)
+        {
+            StopCoroutine(_coroutine);
+            _coroutine = null;
+        }
+
+        if (currentState().endFunc != null)
+        {
+            currentState().endFunc();
+        }
+        
+        _state = state;
+
+        if (currentState().beginFunc != null)
+        {
+            currentState().beginFunc();
+        }
+
+        if (currentState().coroutineFunc != null)
+        {
+            _coroutine = currentState().coroutineFunc();
+            StartCoroutine(_coroutine);
+        }
+    }
+
+    private void Update()
+    {
+        if (currentState().updateFunc == null)
+        {
+            return;
+        }
+
+        int nextState = currentState().updateFunc();
+        TransitionState(nextState);
+    }
+}
